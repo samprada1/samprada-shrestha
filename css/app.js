d6f11be57cec0ecc7009bd1ef0438525b2dc6419
samprada1 <119998487+samprada1@users.noreply.github.com>
2023-02-11 23:30:24 +0545

    Add files via upload

---
+++ css/app.js
@@ -0,0 +1,18 @@
+const body = document.querySelector("body");
+const navbar = document.querySelector(".navbar");
+const menuBtn = document.querySelector(".menu-btn");
+const cancelBtn = document.querySelector(".cancel-btn");
+menuBtn.onclick = ()=>{
+  navbar.classList.add("show");
+  menuBtn.classList.add("hide");     
+  body.classList.add("disabled");
+  // without this the hamburger will not open
+}
+cancelBtn.onclick = ()=>{
+  body.classList.remove("disabled");
+  navbar.classList.remove("show");
+  menuBtn.classList.remove("hide");
+}
+window.onscroll = ()=>{
+  this.scrollY > 50 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
+}
